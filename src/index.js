ba9bfc5f216a3d61d55018dda95c2727643fdbba
frankleitelemoscosta <frankleite090@gmail.com>
2024-08-24 21:53:13 -0300

    feat: add ascii art

--- src/index.js
+++ src/index.js
@@ -13,7 +13,16 @@ app.get("/",(req,res)=>{
 );
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => { console.log("Server running in the port: " + PORT)});
+app.listen(PORT, () => {
+	console.clear();
+	console.log(`
+ ____    _    _   _ _  __     _    ____ ___    ____ _____ _____ _____ _____ 
+| __ )  / \\  | \\ | | |/ /    / \\  |  _ \\_ _|  / ___| ____|  ___| ____|_   _|
+|  _ \\ / _ \\ |  \\| | ' /    / _ \\ | |_) | |  | |   |  _| | |_  |  _|   | |  
+| |_) / ___ \\| |\\  | . \\   / ___ \\|  __/| |  | |___| |___|  _| | |___  | |  
+|____/_/   \\_\\_| \\_|_|\\_\\ /_/   \\_\\_|  |___|  \\____|_____|_|   |_____| |_|  `)
+	console.log("Server running in the port: " + PORT)
+});
 
 app.use('/user', require('./interfaceAdapters/web/controllers/routes/userRouter'));
 app.use('/transaction', require('./interfaceAdapters/web/controllers/routes/transactionRouter'));
