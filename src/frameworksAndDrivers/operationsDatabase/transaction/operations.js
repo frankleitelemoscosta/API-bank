da0db483d6ef785be70d1f43066daa3ddb530541
frankleitelemoscosta <frankleite090@gmail.com>
2024-08-31 14:57:32 -0300

    feat: add menage money in account

--- src/frameworksAndDrivers/operationsDatabase/transaction/operations.js
+++ src/frameworksAndDrivers/operationsDatabase/transaction/operations.js
@@ -1,4 +1,5 @@
 const querys = require('./querys.js');
+const querysUser = require('../user/querys.js');
 const { fillingValues } = require('./fillingValues.js');
 const connection = require('../../dbConfig/DB/config');
 const apiResponse = require('../../../apiCommonResponse/responseApi.js')
@@ -9,7 +10,7 @@ async function getTransaction(data) {
   let results = [];
 
   try {
-      [results] = (await connection.query(querys.get_transaction,[data.CPFpagante]));
+      [results] = (await connection.query(querys.get_transaction,[data.Id_pagante]));
       return apiResponse.successResponse([results]);
     } catch (e) {
       console.log(e);
@@ -17,15 +18,56 @@ async function getTransaction(data) {
     }
    
 }
+
+async function addBalance(Id_Destinatario,value){
+
+  try{
+    let destinatario = (await connection.query(querysUser.get_user,[Id_Destinatario]))
+    destinatario[0].saldo = destinatario[0].saldo + value;
+    await updateUser(destinatario[0]);
+  }catch(e){
+    console.log(e);
+    return apiResponse.internalServerErrorResponse([{}]);
+  }
+
+}
+
+async function subtractBalance(Id_pagante,value){
+
+  try{
+    let pagante = (await connection.query(querysUser.get_user,[Id_pagante]))
+    pagante[0].saldo = pagante[0].saldo - value;
+    await updateUser(pagante[0]);
+  }
+  catch(e){
+    console.log(e);
+    return apiResponse.internalServerErrorResponse([{}]);
+  }  
+}
+
+async function updateUser(data){
+
+  try{
+    await connection.query(querysUser.update_user,[data]);
+    return apiResponse.successResponse([{}]);
+  }catch(e){
+    console.log(e);
+    return apiResponse.internalServerErrorResponse([{}]);
+  }
+
+}
   
 async function insertTransaction(data) {
 
   let result = [];
 
     try {
+      await addBalance(data.Id_Destinatario, data.valor);
+      await subtractBalance(data.Id_pagante, data.valor);
       [result] = (await connection.query(querys.insert_transaction,fillingValues(data)));
       return apiResponse.successResponse([results]);
     } catch (e) {
+      console.log(e);
       return apiResponse.internalServerErrorResponse([{}]);
     }
     
