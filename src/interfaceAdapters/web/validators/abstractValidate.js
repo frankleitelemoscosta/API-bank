707fa76d3b0f933840a8cb0f563fdaca0966e019
frankleitelemoscosta <frankleite090@gmail.com>
2024-08-31 18:51:26 -0300

    fix: fix return data of validate error

--- src/interfaceAdapters/web/validators/abstractValidate.js
+++ src/interfaceAdapters/web/validators/abstractValidate.js
@@ -24,15 +24,18 @@ class AbstractValidate {
     }
 
     CPF(CPF) {
-        if (CPF.replace(/\D/g, '').length !== 11 && !(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/).test(CPF)){
+        console.log(CPF.replace(/\D/g, '').length !== 11)
+        if (CPF.replace(/\D/g, '').length !== 11 || !(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/).test(CPF)){
                 return apiResponse.badRequestResponse({error_message: "Invalid CPF"});
             }
+        return CPF;
     }
 
     Id(id){
         if(Number.isNaN(id)){
             return apiResponse.badRequestResponse({error_message: "Invalid ID"});
         }
+        return id;
     }
 
 }
