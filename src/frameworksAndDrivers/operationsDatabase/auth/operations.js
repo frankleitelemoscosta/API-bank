372860ee4ede435911652db9a8ca0dd19c0bc210
frankleitelemoscosta <frankleite090@gmail.com>
2024-09-01 11:25:07 -0300

    fix: fix error return response.

--- src/frameworksAndDrivers/operationsDatabase/auth/operations.js
+++ src/frameworksAndDrivers/operationsDatabase/auth/operations.js
@@ -5,9 +5,10 @@ async function login(data){
 //vou retornar o user pelo email e ver se a senha bate
 try {
     [results] = (await connection.query(querys.login,[data.email, data.senha]));
-    return apiResponse.successResponse([results]);
+    return results[0];
 } catch (e) {
-    return apiResponse.internalServerErrorResponse([{}]);
+    console.log(e);
+    throw new Error("Request Erro: " + e);
 }
 
 }
