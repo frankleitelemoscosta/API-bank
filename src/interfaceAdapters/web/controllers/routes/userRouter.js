7c967ae401a2670756e5c500ae25b579ee76cdaa
frankleitelemoscosta <frankleite090@gmail.com>
2024-09-01 11:20:32 -0300

    feat: improve the return response per express

--- src/interfaceAdapters/web/controllers/routes/userRouter.js
+++ src/interfaceAdapters/web/controllers/routes/userRouter.js
@@ -13,17 +13,18 @@ router.route('/CreateUser').post(async (req, res) => {
         .setName(bodyRequest.name)
         .setSaldo(bodyRequest.saldo)
         .setEmail(bodyRequest.email)
+        .setSenha(bodyRequest.senha)
         .setTelefone(bodyRequest.telefone)
         .setAniversario(bodyRequest.aniversario)
         .build();
 
         result = await Op.signinUser(newUser);
 
-        res.send(apiResponse.createResponse({data: result, message: 'Usuário criado com sucesso!'}));
+        res.status(201).send(apiResponse.createResponse({data: result, message: 'Usuário criado com sucesso!'}));
         
     }catch(e){
         console.log(e);
-        res.send(apiResponse.internalServerErrorResponse({ message: e.message}));
+        res.status(500).send(apiResponse.internalServerErrorResponse({ message: e.message}));
     }
 
 });
@@ -44,27 +45,29 @@ router.route('/UpdateUser').put(async (req, res) => {
 
         result = await Op.updateUser(user);
 
-        res.send(apiResponse.successResponse({data: result, message: 'Usuário atualizado com sucesso!'}));
+        res.status(200).send(apiResponse.successResponse({data: result, message: 'Usuário atualizado com sucesso!'}));
 
     }catch(e){
         console.log(e);
-        res.send(apiResponse.internalServerErrorResponse({ message: e.message}));
+        res.status(500).send(apiResponse.internalServerErrorResponse({ message: e.message}));
     }
 
 });
 
 router.route('/GetUser').get(async (req, res) => {
     
+    const bodyRequest = req.body;
+
     try{
         const user = new Usuario.Builder()
-        .setCPF(req.query.CPF).build();
+        .setCPF(bodyRequest.CPF).build();
         
         result = await Op.getUser(user);
 
-        res.send(apiResponse.successResponse({data:result, message: 'Usuário encontrado com sucesso!'}));
+        res.status(200).send(apiResponse.successResponse({data:result, message: 'Usuário encontrado com sucesso!'}));
     }catch(e){
         console.log(e);
-        res.send(apiResponse.internalServerErrorResponse({ message: e.message}));   
+        res.status(500).send(apiResponse.internalServerErrorResponse({ message: e.message}));   
     }
 });
 
